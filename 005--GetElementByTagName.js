26edbaf9c3f75fbff240fb9e0354b5b88840e70b
SoniKetan <151501504+SoniKetan@users.noreply.github.com>
2023-11-20 17:04:54 +0530

    GertElementByTagName

---
+++ 005--GetElementByTagName.js
@@ -0,0 +1,8 @@
+let lastItem = document.querySelector(".list-group-item:last-child");
+lastItem.style.color = 'blue';
+
+let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'orange';
+
+let item01 = document.querySelector(".list-group-item");
+item01.style.color = 'red';
