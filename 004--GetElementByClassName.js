18922a00e13b4d9bcd61bb070c2fc2348ed8e09f
SoniKetan <151501504+SoniKetan@users.noreply.github.com>
2023-11-20 15:51:20 +0530

    UseofClassName

---
+++ 004--GetElementByClassName.js
@@ -0,0 +1,15 @@
+
+let uLLTag = document.getElementsByClassName("list-group-item")
+console.log(uLLTag)
+uLLTag[2].style.backgroundColor = 'green';
+
+
+
+for(let i = 0; i < uLLTag.length; i++)
+{
+    uLLTag[i].style.color = 'red';
+}
+
+
+
+
